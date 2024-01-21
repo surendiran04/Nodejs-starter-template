46f2c2453b1442290b96cda870c0758a2d0620cf
surendiran04 <surendiran.m2004@gmail.com>
2024-01-21 12:21:43 +0530

    added files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Nodejs-starter-template
+
+In these we can get a overview of dot env
+
+Using render.com
+
+frontend-backend connectivity also we can include with the help of cors
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 
-require("dotenv").config();
+require("dotenv").config(); //configuring environment variables
 
+
+//processs.env gives us the environment variables
 (process.env.NODE_ENV == "development")?console.log("development environment"):console.log("production environment");
 
-const PORT = (process.env.NODE_ENV == "development")?process.env.NODE_DEV_PORT:process.env.NODE_PROD_PORT;
+//with the help of cors we use the frontend connectivity
+
+const PORT = (process.env.NODE_ENV == "development")?process.env.NODE_DEV_PORT:process.env.NODE_PROD_PORT; 
 
 const HTTP_SERVER = express();
 
